8f42f5d993bbe78151647b9ac08af1ed03ec55c4
ken <sanskar.singh@testrigor.com>
2023-12-17 15:52:02 +0530

    files generated

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format= '[%(asctime)s]: %(message)s')
+
+
+project_name = "textSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/componenets/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb"
+    
+]
+
+for filePath in list_of_files:
+    filePath = Path(filePath)
+    filedir,filename = os.path.split(filePath)
+    
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
+    
+    if (not os.path.exists(filePath)) or (os.path.getsize(filePath) == 0):
+        with open(filePath,'w') as f:
+            logging.info(f"creating file {filePath}")
+            
+    else:
+        logging.info(f"{filename} already exists")
